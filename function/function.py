46c3e88af82259ff94adedba53910b25bed97ef8
HUANG Kaining <1155141441@link.cuhk.edu.hk>
2023-12-11 00:17:54 -0500

    high level function and testing

---
+++ function/function.py
@@ -0,0 +1,20 @@
+def most_upvoted(api_client, post_id):
+    # Retrieve the post
+    post = api_client.runRetrievePostContent(post_id)
+    if not post:
+        return "Post not found", None
+
+    # Retrieve the most upvoted comments under the post
+    top_comments = api_client.runGetTopComments(post_id, 1)
+    if not top_comments:
+        return post, None
+
+    # Expand the most upvoted comment (assuming top_comments is a list)
+    # most_upvoted_comment = top_comments[0]
+
+    # Retrieve the most upvoted reply under the most upvoted comment
+    print("top_comments:", top_comments[0].comment.id)
+    most_upvoted_reply = api_client.runExpandCommentBranch(top_comments[0].comment.id, 1)
+
+    # return post, most_upvoted_reply
+    return most_upvoted_reply
---
+++ test/test_reddit.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import Mock, patch
+import sys
+import os
+
+# Add the 'generated' directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'client')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'function')))
+import client  # Replace with the actual name of your client module
+import function  # Replace with the actual name of your function module
+
+class TestMostUpvotedFunction(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_client = Mock()
+        client.RedditClient = Mock(return_value=self.mock_client)
+
+    def test_most_upvoted_post_not_found(self):
+        self.mock_client.runRetrievePostContent.return_value = None
+
+        result = function.most_upvoted(self.mock_client, "non_existent_post_id")
+        self.assertEqual(result, ("Post not found", None))
+
+    def test_most_upvoted_no_comments(self):
+        self.mock_client.runRetrievePostContent.return_value = "Post Content"
+        self.mock_client.runGetTopComments.return_value = []
+
+        result = function.most_upvoted(self.mock_client, "post234") # post234 has no comments
+        self.assertEqual(result, ("Post Content", None))
+
+    def test_most_upvoted_with_comments_and_replies(self):
+        self.mock_client.runRetrievePostContent.return_value = "Post Content"
+        mock_comment = Mock()
+        mock_comment.comment.id = "comment456"
+        self.mock_client.runGetTopComments.return_value = [mock_comment]
+        mock_reply = Mock()
+        self.mock_client.runExpandCommentBranch.return_value = [mock_reply]
+
+        result = function.most_upvoted(self.mock_client, "post123") # post123 exists; has comments and replies
+        self.assertEqual(result, [mock_reply])
+
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
